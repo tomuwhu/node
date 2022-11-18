671e2e9a77bdb0c1374b8bc86339915757dee8df
Dr. Németh Tamás <tomuwhu@gmail.com>
2022-11-18 07:47:55 +0100

    .

--- static/index.js
+++ static/index.js
@@ -6,5 +6,5 @@ $(() => {
             res => $("#o").html(res.x)
         )
     )
-    $("#i1").mousemove(() => $("#i1o").html($("#i1").val()))
+    $("#i1").mousemove(() => $("#i1o").html(2**$("#i1").val()))
 })
